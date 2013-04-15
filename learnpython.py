55fb323db3aeb3d450437d1368f1f58bcfedd597
stres <stres@stres-P4M90-M7A.(none)>
2013-04-15 16:38:43 +0300

    add ex8

--- learnpython.py
+++ learnpython.py
@@ -175,4 +175,33 @@ name_the_benefits_of_functions()
 
 ###########################
 
+ex8 Classes and Objects
 
+##################
+#define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00 
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+#your code goes here
+MyCar1 = Vehicle()
+MyCar1.name="Fer"
+MyCar1.kind="convertible"
+MyCar1.color="red"
+MyCar1.value= 60000
+
+MyCar2 = Vehicle()
+MyCar2.name="Jump"
+MyCar2.kind="van"
+MyCar2.color="blue"
+MyCar2.value= 10000
+
+#checking code
+print MyCar1.description()
+print MyCar2.description()
+
+##############
