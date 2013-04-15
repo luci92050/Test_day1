594d5b36d65a7403989ce4815ee3724289b5380e
stres <stres@stres-P4M90-M7A.(none)>
2013-04-15 16:22:31 +0300

    add ex2

--- learnpython.py
+++ learnpython.py
@@ -10,3 +10,25 @@ if isinstance(myfloat, float) and myfloat == 10.0:
     print "Float: %d" % myfloat
 if isinstance(myint, int) and myint == 20:
     print "Integer: %d" % myint
+
+ex2:
+
+######
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append('hello')
+strings.append('world')
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+#########
+
